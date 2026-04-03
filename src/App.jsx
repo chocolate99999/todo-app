d7acd582547e011dd570e6db6c6a12e2464db9f1
YR <ladybuglikelei520@gmail.com>
2026-04-03 17:00:47 +0800

    feat: 顯示靜態待辦清單

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 
+const todos = [
+  { id: 1, text: '買牛奶' },
+  { id: 2, text: '寫作業'},
+  { id: 3, text: '運動' },
+]
+
 function App() {
 
   return (
     <div>
       <h1>我的待辦清單</h1>
+
+      <ul>
+        {
+          todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.text}
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
