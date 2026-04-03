fe5c318ae6f20c539d6ce7fbbf08ca27fac3ef01
YR <ladybuglikelei520@gmail.com>
2026-04-03 23:00:49 +0800

    feat: 新增待辦事項功能

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
+import { useState } from "react";
 
-const todos = [
+const initialTodos = [
   { id: 1, text: '買牛奶' },
   { id: 2, text: '寫作業'},
   { id: 3, text: '運動' },
 ]
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+  const [inputValue, setInputValue] = useState('');
+
+  function handleAdd() {
+    if (inputValue.trim() === '')
+      return;
+
+    const newTodo = {
+      id: Date.now(),
+      text: inputValue.trim(),
+    }
+
+    setTodos([...todos, newTodo]);
+    setInputValue('');
+  }
 
   return (
     <div>
       <h1>我的待辦清單</h1>
+
+      <div>
+        <input 
+          type="text"
+          value={inputValue} 
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          placeholder="請輸入待辦事項..."
+        />
+
+        <button onClick={handleAdd}>新增</button>
+      </div>
 
       <ul>
         {
